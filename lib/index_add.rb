ce1a580c4c4e2abbf8e639319e37aa0155ff91f0
Thomas Casady <tscasady@gmail.com>
2023-01-11 12:00:52 -0600

    finish benchmark.

--- lib/index_add.rb
+++ lib/index_add.rb
@@ -1,31 +1,28 @@
 class IndexAdd
-  def initialize(nums, target)
-    @nums = nums
-    @target = target
-  end
 
-  def solution
+  def solution(nums, target)
     solution = []
-    @nums.each_with_index do |num, index|
-      next if num > @target
-      @nums.length.times do |count|
+    nums.each_with_index do |num, index|
+      next if num > target
+      nums.length.times do |count|
         next if index == count
-        solution << index if num + @nums[count] == @target
+        solution << index if num + nums[count] == target
       end
     end
     solution.uniq
   end
 
-  def second_solution
+  def second_solution(nums, target)
     solution = []
-    @nums.each do |num|
-      next if num > @target
+    nums.each do |num|
+      next if num > target
 
-      if @nums.include?(@target - num) && (@target - num != num)
-        solution << @nums.find_index(num)
-        solution << @nums.find_index(@target - num)
+      if nums.include?(target - num) && (target - num != num)
+        solution << nums.find_index(num)
+        solution << nums.find_index(target - num)
+        break
       end
     end
-    solution.uniq
+    solution
   end
 end
--- spec/index_add_spec.rb
+++ spec/index_add_spec.rb
@@ -2,13 +2,21 @@ require './lib/index_add'
 require 'benchmark'
 
 RSpec.describe IndexAdd do
+  let!(:indexer) { IndexAdd.new }
+  let!(:data) {
+      {
+        nums1: {nums: [2, 7, 11, 15], target: 9}, 
+        nums2: {nums: [3, 2, 4], target: 6},
+        nums3: {nums: [12, 42, 31, 23, 41, 23, 452, 12, 5, 2, 123, 432, 1231, 123, 432, 213423, 1234, 1234, 2134, 3451, 1234, 1234, 53141, 543145, 3, 1234, 51345, 1234531, 1, 1345, 134, 3415, 1345, 134, 45, 543, 134], target: 7}
+      }
+    }
   describe 'first solution' do
     it 'given an arrray of numbers it can find which members add up to a target' do
       nums = [2, 7, 11, 15]
 
       target = 9
 
-      expect(IndexAdd.new(nums, target).solution).to eq([0, 1])
+      expect(indexer.solution(nums, target)).to eq([0, 1])
     end
 
     it 'given an arrray of numbers it can find which members add up to a target' do
@@ -16,7 +24,7 @@ RSpec.describe IndexAdd do
 
       target = 6
 
-      expect(IndexAdd.new(nums, target).solution).to eq([1, 2])
+      expect(indexer.solution(nums, target)).to eq([1, 2])
     end
   end
   
@@ -26,7 +34,7 @@ RSpec.describe IndexAdd do
 
       target = 9
 
-      expect(IndexAdd.new(nums, target).second_solution).to eq([0, 1])
+      expect(indexer.solution(nums, target)).to eq([0, 1])
     end
 
     it 'given an arrray of numbers it can find which members add up to a target' do
@@ -34,19 +42,19 @@ RSpec.describe IndexAdd do
 
       target = 6
 
-      expect(IndexAdd.new(nums, target).second_solution).to eq([1, 2])
+      expect(indexer.second_solution(nums, target)).to eq([1, 2])
     end
   end
 
   describe 'benchmarks' do
     it 'outputs benchmarks' do
-      data =  {nums1: {nums: [2, 7, 11, 15], target: 9}, nums2: {nums: [3, 2, 4], target: 6} }
+
       
       data.each do |data, nums|
         puts "First: "
-        puts Benchmark.measure { IndexAdd.new(nums[:nums], nums[:target]).solution}
+        puts Benchmark.measure { indexer.solution(nums[:nums], nums[:target])}
         puts "Second: " 
-        puts Benchmark.measure { IndexAdd.new(nums[:nums], nums[:target]).second_solution}
+        puts Benchmark.measure { indexer.second_solution(nums[:nums], nums[:target])}
         puts "\n"
       end
     end
